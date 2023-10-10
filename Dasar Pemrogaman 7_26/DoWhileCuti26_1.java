0e3de78b410fdcc32579f8446c8c0477aa873161
SatriaNadirakhma <144087759+SatriaNadirakhma@users.noreply.github.com>
2023-10-10 17:32:00 +0700

    Percobaan 3_Modif 1_Satria R_26
    Percobaan 3_Modif 1_Satria R_26

---
+++ Dasar Pemrogaman 7_26/DoWhileCuti26_1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class DoWhileCuti26_1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int jatahCuti,jumlahHari;
+        String konfirmasi;
+
+        System.out.print("Jatah cuti: ");
+        jatahCuti = scan.nextInt();
+
+        do {
+            System.out.print("Apakah anda ingin mengambil cuti (y/t)? ");
+            konfirmasi = scan.next();
+
+            if (konfirmasi.equalsIgnoreCase("y")){
+                System.out.print("Jumlah hari: ");
+                jumlahHari = scan.nextInt();
+
+                if(jumlahHari <= jatahCuti){
+                    jatahCuti -= jumlahHari;
+                    System.out.println("Sisa jatah cuti: " +jatahCuti);
+                } else {
+                    System.out.println("Sisa jatah cuti anda tidak mencukupi");
+                }
+            }
+
+        } while (jatahCuti > 0);
+    }
+    
+}
