06c2fcb25663a02e30f624499e14f0221555eee3
SatriaNadirakhma <144087759+SatriaNadirakhma@users.noreply.github.com>
2023-10-10 16:05:24 +0700

    Percobaan 1_Modif1_Satria R_26
    Percobaan 1 Modifikasi 1_26

---
+++ Dasar Pemrogaman 7_26/ForKelipatan26_1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ForKelipatan26_1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner (System.in);
+        
+        int kelipatan;
+        int jumlah = 0;
+        int counter = 0;
+        double average = 0.0;
+
+        System.out.print("Masukkan Bilangan Kelipatam (1-9): ");
+        kelipatan = scan.nextInt();
+
+        for (int i = 1; i <= 50; i++) {
+            if (i % kelipatan == 0) {
+                jumlah += i;
+                counter++;
+            }
+        }
+            if (counter > 0) {
+                average = (double) jumlah / counter;
+            }
+        System.out.printf("Banyaknya Bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
+        System.out.printf("Total Bilangan Kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+        System.out.printf("Rata-rata bilangan kelipatan %d dari 1 sampai 50 adalah %.2f\n", kelipatan, average);
+    }
+}
+
+
